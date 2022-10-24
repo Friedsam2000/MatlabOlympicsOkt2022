2adf277d629beb92944bcf5018924d663ebf639a
unknown <paola.canuto@tum.de>
2022-10-24 11:44:20 +0200

    add solution

--- Paola/taylorseries.m
+++ Paola/taylorseries.m
@@ -1,12 +1,4 @@
 function n = taylorseries(x)
-    ex = 0;
-    error = abs(exp(x) - ex);
-    
-    n = -1; 
-    while error > 0.001
-        n = n + 1;
-        ex = ex + (x^n)/factorial(n);
-        error = abs(exp(x) - ex);
-    end
+
 
 end
