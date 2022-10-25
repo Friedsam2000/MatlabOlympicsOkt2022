79c17ee0062c523cfe5ca071f9dc61c7ec41a949
unknown <paola.canuto@tum.de>
2022-10-25 13:06:46 +0200

    corrected answer

--- Lösungsvorschlag/taylorseries.m
+++ Lösungsvorschlag/taylorseries.m
@@ -2,11 +2,11 @@ function requiredTerms = taylorseries(x)
     ex = 0;
     error = abs(exp(x) - ex);
     
-    requiredTerms = -1; 
+    requiredTerms = 0; 
     while error > 0.001
-        requiredTerms = requiredTerms + 1;
         ex = ex + (x^requiredTerms)/factorial(requiredTerms);
         error = abs(exp(x) - ex);
+        requiredTerms = requiredTerms + 1;
     end
 
 end
