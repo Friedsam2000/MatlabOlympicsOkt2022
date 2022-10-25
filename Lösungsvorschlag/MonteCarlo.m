774e804f9e2682132de3d34a48b8c14465d281b7
unknown <paola.canuto@tum.de>
2022-10-25 10:57:59 +0200

    explanation toolbox

--- Lösungsvorschlag/MonteCarlo.m
+++ Lösungsvorschlag/MonteCarlo.m
@@ -1,12 +1,32 @@
 function [mean_approx,var_approx] = MonteCarlo(Mittelwert,Varianz)
-%% 1) Erstellt eine normalverteilte Wahrscheinlichkeitsverteilung "pd"
+%% 1) Statistics and Machine Learining Toolbox installieren
+% Um diese Übung durchzuführen, brauchst du die Statistics and Machine 
+% Learning Toolbox. Die Toolbox ist eine Sammlung von Funktionen, die auf 
+% der technischen Berechnungsumgebung MATLAB® aufbauen. Mit deiner TUM-Lizenz 
+% kannst du sie alle kostenlos installieren.
+
+% Schritte zur Installation: 
+% a) Speicher alle deine MATLAB-Dateien (du musst MATLAB später neu starten)
+% b) Gehe auf 'Home', im Bereich 'Environment' findest du die Schaltfläche 
+%    'Add-Ons'. Wenn du darauf klickst, erscheint eine neue Registerkarte 
+%    mit allen zusätzlichen MATLAB-Produkten, die du installieren kannst.
+% c) Suche in der oberen rechten Ecke nach Statistics and Machine Learning 
+%    Toolbox.
+% d) Wenn du zur Toolbox-Seite gelangst, klicke auf Install und folge den 
+%    Anweisungen.
+% e) MATLAB wird neu gestartet! Vergiss nicht, den Ordner mit den Dateien, 
+%    die du verwendest, erneut zum Path hinzuzufügen (Rechtsklick -> 
+%    Add to Path -> Select Folders and Subfolders)
+
+
+%% 2) Erstellt eine normalverteilte Wahrscheinlichkeitsverteilung "pd"
 % mit dem Mittelwert und der Varianz, die der Funtkion übergeben werden.
 % Befehl: makedist (-> doc makedist ) 
 % Achtung: Die Standardabweichung ist die Wurzel der Varianz
 
 pd = makedist('Normal','mu',Mittelwert,'sigma',sqrt(Varianz));
 
-%% 2) Erstellt eine for-Schleife und ziehe 10000 mal aus der Verteilung
+%% 3) Erstellt eine for-Schleife und ziehe 10000 mal aus der Verteilung
 % Befehl: sample = random(pd)
 % Speichert alle samples in einem Vektor
 
@@ -24,7 +44,7 @@ end
 % Das ist eigentlich auch die bevorzugte und effizientere Methode.
 % samples = random(pd, [1,10000]);
 
-%% 3) Berechnet umgekehrt den Mittelwert und Varianz aus dem sample-Vektor
+%% 4) Berechnet umgekehrt den Mittelwert und Varianz aus dem sample-Vektor
 % Befehle: mean, std oder var
 
 mean_approx = mean(samples);
--- Olympiade/MonteCarlo.m
+++ Olympiade/MonteCarlo.m
@@ -1,17 +1,36 @@
 function [mittelwert_approx,varianz_approx] = MonteCarlo(Mittelwert,Varianz)
-%% 1) Erstellt eine Normalverteilung "pd" % mit dem Mittelwert und der Varianz, die der Funktion übergeben werden.
+%% 1) Statistics and Machine Learining Toolbox installieren
+% Um diese Übung durchzuführen, brauchst du die Statistics and Machine 
+% Learning Toolbox. Die Toolbox ist eine Sammlung von Funktionen, die auf 
+% der technischen Berechnungsumgebung MATLAB® aufbauen. Mit deiner TUM-Lizenz 
+% kannst du sie alle kostenlos installieren.
+
+% Schritte zur Installation: 
+% a) Speicher alle deine MATLAB-Dateien (du musst MATLAB später neu starten)
+% b) Gehe auf 'Home', im Bereich 'Environment' findest du die Schaltfläche 
+%    'Add-Ons'. Wenn du darauf klickst, erscheint eine neue Registerkarte 
+%    mit allen zusätzlichen MATLAB-Produkten, die du installieren kannst.
+% c) Suche in der oberen rechten Ecke nach Statistics and Machine Learning 
+%    Toolbox.
+% d) Wenn du zur Toolbox-Seite gelangst, klicke auf Install und folge den 
+%    Anweisungen.
+% e) MATLAB wird neu gestartet! Vergiss nicht, den Ordner mit den Dateien, 
+%    die du verwendest, erneut zum Path hinzuzufügen (Rechtsklick -> 
+%    Add to Path -> Select Folders and Subfolders)
+
+%% 2) Erstellt eine Normalverteilung "pd" % mit dem Mittelwert und der Varianz, die der Funktion übergeben werden.
 % Befehl: makedist (-> doc makedist ) 
 % Achtung: Die Standardabweichung ist die Wurzel aus der Varianz
 
 
 
-%% 2) Erstellt eine for-Schleife und ziehe 10000 mal aus der Verteilung
+%% 3) Erstellt eine for-Schleife und ziehe 10000 mal aus der Verteilung
 % Befehl: sample = random(pd)
 % Speichert alle samples in einem Vektor
 
 
 
-%% 3) Berechnet umgekehrt den Mittelwert und Varianz aus dem sample-Vektor
+%% 4) Berechnet umgekehrt den Mittelwert und Varianz aus dem sample-Vektor
 % Befehle: mean, std/var
 
 mittelwert_approx = []; % überschreiben
